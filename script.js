152ee1970b2602536440c7438b94b44850b0d571
sashamoon <sarah.alves8@hotmail.com>
2021-12-16 17:20:08 -0300

    :package: Inserção pesquisar por data e footer

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function imagemHoje(){
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=xo7gH5Pw64xhWZkhgw4rvdZiBB6IoCztrMynSwRh`,
         success: function(response){
-            $(".img-background").html(`<img src="${response.hdurl}" alt="Foto do espaço hoje">`); //Header
-            $(".titulo-ajax").html(`${response.title}`); //Titulo header
-            $(".img-dia").html(`<img src="${response.hdurl}" class="img-hoje" alt="Foto do espaço hoje">`);
-
+            if(response.media_type == "image"){
+                $(".img-background").html(`<img src="${response.hdurl}" class="img-hoje" alt="Foto do espaço hoje">`);
+            } else {
+                $(".img-background").html(`<img src="./imagens/background_temporario.jpg" class="img-hoje" alt="Foto do espaço hoje">`);
+            }
+            $(".titulo-ajax").text(`${response.title}`); //Titulo header
+            $(".data-ajax").text(`${response.date}`); //Data section imagem-dia
+            if(response.media_type == "image"){
+                $(".img-dia").html(`<img src="${response.hdurl}" class="img-hoje" alt="Foto do espaço hoje">`);
+            } else {
+                $(".img-dia").html(`<video src="${response.hdurl}" autoplay controls class="img-hoje" alt="Foto do espaço hoje"></video`);
+            }
+            $(".descricao-ajax").text(`${response.explanation}`); //Data section imagem-dia
+            $(".result-pesquisa").css("display", "none");
         },
         error: function(error){
             console.error(error);
@@ -20,3 +30,33 @@ function imagemHoje(){
     });
 }
 imagemHoje();
+
+function filtrarPorData(){
+    $('button').click(function(){
+        let data = $("#data").val();
+        $.ajax({
+            url: `https://api.nasa.gov/planetary/apod?api_key=xo7gH5Pw64xhWZkhgw4rvdZiBB6IoCztrMynSwRh&date=${data}`,
+            success: function(response){
+                $(".result-pesquisa").css("display", "block");
+                $(".titulo-ajax-pesquisa").text(`${response.title}`); //Titulo header
+                $(".titulo-ajax-pesquisa").css("color", 'white');
+                $(".data-ajax-pesquisa").text(`${response.date}`); //Data section imagem-dia
+                if(response.media_type == "image"){
+                    $(".img-dia-pesquisa").html(`<img src="${response.hdurl}" class="img-hoje" alt="Foto do espaço hoje">`);
+                } else {
+                    $(".img-dia-pesquisa").html(`<iframe width="420" height="315" src="${response.url}?autoplay=1&mute=1"></iframe>`);
+                }
+                $(".descricao-ajax-pesquisa").text(`${response.explanation}`); //Data section imagem-dia
+                $(".imagem-dia_descricao").css("display", "none"); //Data section imagem-dia
+            },
+            error: function(error){
+                console.error(error);
+                $(".titulo-ajax-pesquisa").text(`Não foi possivel encontrar essa data, tente uma que exista!`); //Titulo header
+                $(".titulo-ajax-pesquisa").css("color", 'red'); //Titulo header
+                $(".result-pesquisa").css("display", 'none');
+                $(".btn-pesquisa").css("border-color", 'red');
+            }
+        });
+    });
+}
+filtrarPorData();
